0f6e045abfaed424062c017004079671a0b0323a
Sergey Yakupov <motherboard98@yandex.ru>
2024-10-27 18:12:40 +0300

    ++

--- sorter.py
+++ sorter.py
@@ -157,59 +157,111 @@ class Sorter():
         return escd_dict
     
 
-    def move_files_to_folders(self, destination_folder):
-        """
-        Распределение файлов по папкам на основе префикса, системы ЕСКД РФ и обработки неправильных форматов.
-        """
-        file_names = self.get_file_names()
-        seen = {}
-        escd_dict = self.create_escd_dict("ESCD.xlsx")
-        for file_name in file_names:
-            prefix = self.extract_prefix(file_name)
-            lat_file_name = self.replace_cyrillic_to_latin(file_name)
+    # def move_files_to_folders(self, destination_folder):
+    #     """
+    #     Распределение файлов по папкам на основе префикса, системы ЕСКД РФ и обработки неправильных форматов.
+    #     """
+    #     file_names = self.get_file_names()
+    #     seen = {}
+    #     escd_dict = self.create_escd_dict("ESCD.xlsx")
+    #     for file_name in file_names:
+    #         prefix = self.extract_prefix(file_name)
+    #         lat_file_name = self.replace_cyrillic_to_latin(file_name)
 
-            if prefix:
-                project_code = prefix[:5]  
-                project_number = prefix[6:]  #
+    #         if prefix:
+    #             project_code = prefix[:5]  
+    #             project_number = prefix[6:]  #
 
-                if re.match(r'^HB\d{3}\.\d{6}', prefix):
-                    target_folder = os.path.join(destination_folder, project_code)  
+    #             if re.match(r'^HB\d{3}\.\d{6}', prefix):
+    #                 target_folder = os.path.join(destination_folder, project_code)  
 
     
-                    for i in range(2, len(project_number) + 1):
-                        sub_folder = project_number[:i]  
+    #                 for i in range(2, len(project_number) + 1):
+    #                     sub_folder = project_number[:i]  
                         
-                        if sub_folder in escd_dict:
-                            folder_name = f"{sub_folder} - {escd_dict[sub_folder]}" 
-                        else:
-                            folder_name = sub_folder 
-                        target_folder = os.path.join(target_folder, folder_name)
-                else:
-                    target_folder = os.path.join(destination_folder, project_code, "Прочие файлы")
+    #                     if sub_folder in escd_dict:
+    #                         folder_name = f"{sub_folder} - {escd_dict[sub_folder]}" 
+    #                     else:
+    #                         folder_name = sub_folder 
+    #                     target_folder = os.path.join(target_folder, folder_name)
+    #             else:
+    #                 target_folder = os.path.join(destination_folder, project_code, "Прочие файлы")
             
+    #         elif re.match(r'^HB\d{3}-\d{3}', lat_file_name):
+    #             project_code = lat_file_name[:5]  
+    #             target_folder = os.path.join(destination_folder, project_code, "ОССЗ.Письма проектировщика")
+            
+    #         elif re.match(r'^120-\d{3}', lat_file_name):
+    #             target_folder = os.path.join(destination_folder, "ОССЗ", "Письма")
+            
+    #         else:
+    #             target_folder = os.path.join(destination_folder, "Прочие документы")
+            
+    #         extension = os.path.splitext(file_name)[1][1:].lower()
+    #         if extension:
+    #             target_folder = os.path.join(target_folder, extension)
+
+    #         if not os.path.exists(target_folder):
+    #             os.makedirs(target_folder)
+
+    #         src_path = os.path.join(self.folder_path, file_name)
+    #         dest_path = os.path.join(target_folder, file_name)
+
+    #         if file_name not in seen:
+    #             shutil.copy(src_path, dest_path)  
+    #             seen[file_name] = True
+    def move_files_to_folders(self, destination_folder):
+        file_names = self.get_file_names()
+        escd_dict = self.create_escd_dict("ESCD.xlsx")
+        seen = {}
+
+        for file_name in file_names:
+            lat_file_name = self.replace_cyrillic_to_latin(file_name)
+            prefix = self.extract_prefix(file_name)
+            
+            if prefix:
+                target_folder = self.handle_prefix_case(destination_folder, prefix, escd_dict)
             elif re.match(r'^HB\d{3}-\d{3}', lat_file_name):
-                project_code = lat_file_name[:5]  
-                target_folder = os.path.join(destination_folder, project_code, "ОССЗ.Письма проектировщика")
-            
-            elif re.match(r'^120-\d{3}', lat_file_name):
-                target_folder = os.path.join(destination_folder, "ОССЗ", "Письма")
-            
+                target_folder = self.handle_specific_format_case(destination_folder, lat_file_name, "ОССЗ.Письма проектировщика")
+                if re.match(r'^120-\d{3}', lat_file_name):
+                    target_folder = self.handle_specific_format_case(destination_folder, lat_file_name, "ОССЗ.Письма")
             else:
                 target_folder = os.path.join(destination_folder, "Прочие документы")
-            
-            extension = os.path.splitext(file_name)[1][1:].lower()
-            if extension:
-                target_folder = os.path.join(target_folder, extension)
 
-            if not os.path.exists(target_folder):
-                os.makedirs(target_folder)
+            target_folder = self.append_extension_folder(target_folder, file_name)
+            self.copy_file_to_folder(file_name, target_folder, seen)
 
-            src_path = os.path.join(self.folder_path, file_name)
-            dest_path = os.path.join(target_folder, file_name)
+    def handle_prefix_case(self, destination_folder, prefix, escd_dict):
+        project_code = prefix[:5]
+        project_number = prefix[6:]
+        target_folder = os.path.join(destination_folder, project_code)
 
-            if file_name not in seen:
-                shutil.copy(src_path, dest_path)  
-                seen[file_name] = True
+        for i in range(2, len(project_number) + 1):
+            sub_folder = project_number[:i]
+            folder_name = f"{sub_folder} - {escd_dict.get(sub_folder, sub_folder)}"
+            target_folder = os.path.join(target_folder, folder_name)
 
+        return target_folder
+
+    def handle_specific_format_case(self, destination_folder, file_name, specific_folder):
+        project_code = file_name[:5]
+        return os.path.join(destination_folder, project_code, specific_folder)
+
+    def append_extension_folder(self, target_folder, file_name):
+        extension = os.path.splitext(file_name)[1][1:].lower()
+        if extension:
+            target_folder = os.path.join(target_folder, extension)
+        return target_folder
+
+    def copy_file_to_folder(self, file_name, target_folder, seen):
+        if not os.path.exists(target_folder):
+            os.makedirs(target_folder)
+
+        src_path = os.path.join(self.folder_path, file_name)
+        dest_path = os.path.join(target_folder, file_name)
+
+        if file_name not in seen:
+            shutil.copy(src_path, dest_path)
+            seen[file_name] = True
 
         
